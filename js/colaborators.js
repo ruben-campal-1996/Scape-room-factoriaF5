d042cb7e4cc2f17911b5b42080bc3be39fe01d39
Jose Luis <jlcgmieres15@gmail.com>
2026-04-16 13:51:45 +0200

    creating buttons container

--- js/colaborators.js
+++ js/colaborators.js
@@ -3,21 +3,25 @@ const creator = [
     name: "Ada Florido",
     img: "https://avatars.githubusercontent.com/u/260862430?v=4",
     url: "https://github.com/aflorido266",
+    linkedin: "",
   },
   {
     name: "Jose Luis Castro García",
     img: "https://avatars.githubusercontent.com/u/99686037?v=4",
     url: "https://github.com/Josecgh",
+    linkedin: "https://www.linkedin.com/in/jose-luis-castro-garc%C3%ADa-545b4a320/",
   },
   {
     name: "Ruben Campal López",
     img: "https://avatars.githubusercontent.com/u/195755063?s=48&v=4",
     url: "https://github.com/ruben-campal-1996",
+    linkedin: "https://www.linkedin.com/in/rubén-campal-lópez-10036627b/",
   },
   {
     name: "Ruddy Pamela Cruz Campoverde",
     img: "https://avatars.githubusercontent.com/u/260880616?v=4",
     url: "https://github.com/ruddycruzc",
+    linkedin: "www.linkedin.com/in/ruddycruzc",
   }
 ];
 
--- js/event-colab.js
+++ js/event-colab.js
@@ -5,9 +5,9 @@ const main_container = document.querySelector(".main-container");
 const container_creators = document.createElement("section");
 container_creators.className = "creators-container";
 
-const title = document.createElement("h2");
+const title = document.createElement("h1");
 title.textContent = "Supervivientes de oro";
-container_creators.appendChild(title); 
+main_container.appendChild(title); 
 
 main_container.appendChild(container_creators);
 
@@ -22,15 +22,25 @@ function mostrarCreadores() {
 
     const img_creator = document.createElement("img");
     img_creator.src = person.img;
-    img_creator.alt = person.name; // Good for accessibility!
+    img_creator.alt = person.name;
     img_creator.className = "img-user";
     creatorCard.appendChild(img_creator);
 
+    const socials_creator = document.createElement("section");
+    socials_creator.className = "socials-container";
+    creatorCard.appendChild(socials_creator);
+
     const url_creador = document.createElement("a");
     url_creador.href = person.url;
-    url_creador.className = "creator-link";
+    url_creador.classList.add('creator-link', 'btn-git')
     url_creador.textContent = "Ver perfil"; 
-    creatorCard.appendChild(url_creador);
+    socials_creator.appendChild(url_creador);
+
+    const linkedIn = document.createElement("a");
+    linkedIn.href = person.linkedin;
+    linkedIn.classList.add('creator-link', 'btn-linkedin');
+    linkedIn.textContent = "Ver Linkedin"; 
+    socials_creator.appendChild(linkedIn);
 
     container_creators.appendChild(creatorCard);
   });
