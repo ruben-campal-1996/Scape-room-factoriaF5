54b841e65d591a12f80f0cbb82fad5e49736d5f5
Jose Luis <jlcgmieres15@gmail.com>
2026-04-16 11:13:50 +0200

    correct link contact us

---
+++ js/colaborators.js
@@ -0,0 +1,24 @@
+const creator = [
+  {
+    name: "Ada Florido",
+    img: "https://avatars.githubusercontent.com/u/260862430?v=4",
+    url: "https://github.com/aflorido266",
+  },
+  {
+    name: "Jose Luis Castro García",
+    img: "https://avatars.githubusercontent.com/u/99686037?v=4",
+    url: "https://github.com/Josecgh",
+  },
+  {
+    name: "Ruben Campal López",
+    img: "https://avatars.githubusercontent.com/u/195755063?s=48&v=4",
+    url: "https://github.com/ruben-campal-1996",
+  },
+  {
+    name: "Ruddy Pamela Cruz Campoverde",
+    img: "https://avatars.githubusercontent.com/u/260880616?v=4",
+    url: "https://github.com/ruddycruzc",
+  }
+];
+
+export {creator};
