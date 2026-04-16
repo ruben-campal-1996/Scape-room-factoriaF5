36301098e5333188e75a4f5e223aa032d5b9d9c8
Rubén Campal <ruben.campal.lopez1996@gmail.com>
2026-04-16 15:37:42 +0200

    add logic and styles for 1st screen

--- js/reto2.js
+++ js/reto2.js
@@ -1,16 +1,24 @@
-const main = document.querySelector("main")
+function limpiarPantalla() {
+    const main= document.querySelector("main")
+    main.innerHTML =""
+}
 
-const nuevaSection = document.createElement("section")
-const divGeneral = document.createElement("div")
-const imgContainer = document.createElement("div")
-const img = document.createElement("img")
-const h1 = document.createElement("h1")
-const textContainer = document.createElement("div")
-const textos = [
-    "Te vas a abrir paso a traves de la puerta, pero escuchas algo... ¡AGUANTA LA PUERTA!",
+function escena1Reto2(){
+    const main = document.querySelector("main")
+    
+    const nuevaSection = document.createElement("section")
+    const divGeneral = document.createElement("div")
+    const imgContainer = document.createElement("div")
+    const img = document.createElement("img")
+    const h1 = document.createElement("h1")
+    const textContainer = document.createElement("div")
+    const textos = [
+        "Te vas a abrir paso a traves de la puerta, pero escuchas algo...",
+        "¡AGUANTA LA PUERTA!",
     "Son muchos, y no podrás resistir eternamente... pero recuerdas que detrás tuyo había una mesa.",
-    "Que deberías coger para sobrevivir?"
+    "¿Que deberías coger para sobrevivir?"
 ]
+const boton = document.createElement("button")
 
 
 nuevaSection.id = "reto2"
@@ -18,8 +26,10 @@ divGeneral.classList.add("general-container")
 imgContainer.classList.add("img-container")
 textContainer.classList.add("text-container")
 h1.textContent="¡HAS DESCRIFADO LA CONTRASEÑA!"
+boton.classList.add("btn-start-r2")
+boton.textContent="Escoger objeto"
 
-textos.forEach(texto => {
+textos.forEach(texto => {   //Introducir los párrafos dentro del div contenedor
     const p = document.createElement("p")
     p.textContent = texto
     textContainer.appendChild(p) 
@@ -34,4 +44,12 @@ divGeneral.appendChild(imgContainer)
 divGeneral.appendChild(h1)
 divGeneral.appendChild(textContainer)
 imgContainer.appendChild(img)
+divGeneral.appendChild(boton)
 
+boton.addEventListener("click", () => {
+    limpiarPantalla()
+})
+}
+
+
+escena1Reto2()
