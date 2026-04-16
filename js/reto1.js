1af6736fbd4406a24600d0b45d4d0dcbde5038b1
Ada <aflorido266@gmail.com>
2026-04-16 17:28:21 +0200

    add DOM de pantallas: Correcto o Incorrecto

--- js/reto1.js
+++ js/reto1.js
@@ -20,29 +20,57 @@ export function reto1() {
     titulo2.textContent = "RESUELVE PARA CONTINUAR";
 
     const texto2 = document.createElement("p");
-    texto2.textContent = "(14 + 6) * (22 + 1) * (20/4) + (30 *15)";
+    texto2.textContent = "(14 + 6) * (22 + 1) * (20/4) + (30 * 15)";
 
-    const dig1 = document.createElement("input");
-    const dig2 = document.createElement("input");
-    const dig3 = document.createElement("input");
-    const dig4 = document.createElement("input");
+    // inputs
+    const i1 = document.createElement("input");
+    const i2 = document.createElement("input");
+    const i3 = document.createElement("input");
+    const i4 = document.createElement("input");
 
+    i1.maxLength = 1;
+    i2.maxLength = 1;
+    i3.maxLength = 1;
+    i4.maxLength = 1;
+
+    // botón comprobar
     const comprobar = document.createElement("button");
-    comprobar.textContent = "INTRODUCE CÓDIGO";
+    comprobar.textContent = "COMPROBAR";
 
+    // resultado
     const resultado = document.createElement("p");
 
     comprobar.addEventListener("click", () => {
-      if (input.value === "2750") {
-        resultado.textContent = "✔ Correcto, puerta abierta";
+      const codigo = i1.value + i2.value + i3.value + i4.value;
+
+      if (codigo === "2750") {
+        reto1.innerHTML = "";
+        const correcto = document.createElement("h2");
+        correcto.textContent = "!CODIGO CORRECTO!";
+      
+        const botonOk = document.createElement("button");
+        botonOk.textContent = "SIGUIENTE"
+
+        reto1.appendChild(correcto)
+        reto1.appendChild(botonOk)
+
       } else {
-        resultado.textContent = "❌ Incorrecto";
+        reto1.innerHTML = "";
+        const incorrecto = document.createElement("h2");
+        incorrecto.textContent = "!CODIGO INCORRECTO!";
+        const botonNook = document.createElement("button");
+        botonNook.textContent = "REINTENTAR";     
+        reto1.appendChild(incorrecto);
+        reto1.appendChild(botonNook)
       }
     });
 
     reto1.appendChild(titulo2);
     reto1.appendChild(texto2);
-    reto1.appendChild(dig1);
+    reto1.appendChild(i1);
+    reto1.appendChild(i2);
+    reto1.appendChild(i3);
+    reto1.appendChild(i4);
     reto1.appendChild(comprobar);
     reto1.appendChild(resultado);
   });
