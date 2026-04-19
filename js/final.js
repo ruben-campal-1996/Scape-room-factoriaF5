70ae76923b21f5838b334b51ff403d403c9418df
Rubén Campal <ruben.campal.lopez1996@gmail.com>
2026-04-19 18:52:27 +0200

    fix styles good end

--- js/final.js
+++ js/final.js
@@ -15,17 +15,20 @@ function mostrarFinalBueno() {
   const img = document.createElement("img");
   img.src = "../assets/img/resources/salida-scape.png";
   img_container.appendChild(img);
+  const contentContainer = document.createElement("div");
+  contentContainer.classList.add("content-container");
+  hero.appendChild(contentContainer);
   const h1 = document.createElement("h1");
-  h1.textContent = "¡Felicidades!";
-  hero_container.appendChild(h1);
+  h1.textContent = "¡FELICIDADES!";
+  contentContainer.appendChild(h1);
   const parraph = document.createElement("p");
   parraph.textContent = "Has logrado escapar.";
-  hero_container.appendChild(parraph);
+  contentContainer.appendChild(parraph);
   const a_return = document.createElement("a");
   a_return.href = "../index.html";
   a_return.className = "btn-return";
   a_return.textContent = "Volver al inicio.";
-  hero_container.appendChild(a_return);
+  contentContainer.appendChild(a_return);
 }
 
 function mostrarFinalMalo() {
