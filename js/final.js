5e07670b88c3b4109c00094be1c00bbc99fcf3cd
Jose Luis <jlcgmieres15@gmail.com>
2026-04-22 11:17:21 +0200

    updating the final

--- js/final.js
+++ js/final.js
@@ -1,4 +1,4 @@
-const estado = localStorage.getItem("estadoJuego");
+const estado = localStorage.getItem("ganado");
 
 const main = document.querySelector(".main-container");
 const hero = document.createElement("section");
@@ -19,16 +19,18 @@ function mostrarFinalBueno() {
   contentContainer.classList.add("content-container");
   hero.appendChild(contentContainer);
   const h1 = document.createElement("h1");
-  h1.textContent = "¡FELICIDADES!";
+  h1.className = "h1-final";
+  h1.textContent = "EL MUNDO EXTERIOR DA MIEDO";
   contentContainer.appendChild(h1);
   const parraph = document.createElement("p");
-  parraph.textContent = "Has logrado escapar.";
+  parraph.textContent = "¿TE ATREVES A INTENTARLO DE NUEVO?";
   contentContainer.appendChild(parraph);
   const a_return = document.createElement("a");
   a_return.href = "../index.html";
   a_return.className = "btn-return";
   a_return.textContent = "Volver al inicio.";
   contentContainer.appendChild(a_return);
+  
 }
 
 function mostrarFinalMalo() {
@@ -42,6 +44,7 @@ function mostrarFinalMalo() {
   contentContainer.classList.add("content-container");
   hero.appendChild(contentContainer)
   const h1 = document.createElement("h1");
+  h1.className = "h1-final";
   h1.textContent = "¡HAS PERDIDO!";
   contentContainer.appendChild(h1);
   const parraph = document.createElement("p");
