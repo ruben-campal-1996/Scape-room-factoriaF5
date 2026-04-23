8b64e3718e90ef646dee2847e6ad48b143eb5725
Ruddy <ruddy391@gmail.com>
2026-04-23 13:57:38 +0200

    Fix css reto3

--- js/reto3.js
+++ js/reto3.js
@@ -36,12 +36,12 @@ class JuegoReto3 {
       new Maleta(
         1,
         "../assets/img/resources/maleta-abierta.png",
-        "Portátil, Auriculares y mouse...Siempre hay tiempo para analizar código, ¿no?",
+        "Portátil, Auriculares y manual de git...Siempre hay tiempo para analizar código, ¿no?",
         false,
         [
           "../assets/img/resources/portatil.png",
           "../assets/img/resources/auriculares.png",
-          "../assets/img/resources/mouse.png",
+          "../assets/img/resources/libro-sagrado.png",
         ],
       ),
       new Maleta(
@@ -163,7 +163,7 @@ document.addEventListener("touchmove", this._touchHandler);
 
   mostrarSeleccion() {
     this.contenedor.innerHTML = "";
-    this.contenedor.className = "pantalla-contexto";
+    this.contenedor.className = "pantalla-contexto pantalla-seleccion";
 
     const imgContainer = document.createElement("div");
     imgContainer.classList.add("sala-fondo");
@@ -223,7 +223,7 @@ document.addEventListener("touchmove", this._touchHandler);
    this.desactivarProximidad();
 
     this.contenedor.innerHTML = "";
-    this.contenedor.className = "pantalla-contexto";
+    this.contenedor.className = "pantalla-contexto pantalla-maleta";
 
     
   
@@ -240,12 +240,6 @@ document.addEventListener("touchmove", this._touchHandler);
 
     maletaWrapper.appendChild(img);
 
-    const posiciones = [
-  { top: "55%", left: "30%" },
-  { top: "50%", left: "50%" },
-  { top: "50%", left: "70%" }
-];
-
 const objetosContainer = document.createElement("div");
 objetosContainer.classList.add("objetos-container");
 
@@ -254,10 +248,11 @@ maleta.objetos.forEach((ruta, i) => {
   obj.src = ruta;
   obj.classList.add("objeto");
 
-  obj.style.animationDelay = `${i * 0.2}s`;
+  obj.style.animationDelay = `${i * 0.15}s`;
 
   objetosContainer.appendChild(obj);
 });
+
 
 maletaWrapper.appendChild(objetosContainer);
 
@@ -285,9 +280,14 @@ maletaWrapper.appendChild(objetosContainer);
       this.comprobarResultado(maleta);
     });
 
-    textoContainer.appendChild(texto);
-    textoContainer.appendChild(botonElegir);
-    textoContainer.appendChild(botonVolver);
+const botonesContainer = document.createElement("div");
+botonesContainer.classList.add("boton-container");
+
+botonesContainer.appendChild(botonElegir);
+botonesContainer.appendChild(botonVolver);
+
+textoContainer.appendChild(texto);
+textoContainer.appendChild(botonesContainer);
 
     this.contenedor.appendChild(imgContainer);
     this.contenedor.appendChild(textoContainer);
